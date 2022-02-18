494906404c18aa11d46ae23a34c29e0e18495d77
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-18 13:46:00 +1100

    Connect to the DB and update genres endpoints to use DB instead of static array

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const config = require('config');
 const logger = require('./middleware/logger');
 const auth = require('./middleware/auth');
 const genres = require('./routes/genres');
@@ -8,6 +10,13 @@ const homepage = require('./routes/homepage');
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const password = config.get('database.password');
+const dbConnect = `mongodb+srv://root:${password}@playground.mryia.mongodb.net/vidly?retryWrites=true&w=majority`;
+mongoose.connect(dbConnect)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('error', err.message));
+
 
 app.set('view engine', 'pug');
 
