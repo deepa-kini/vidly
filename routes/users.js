d4ca062bbab10212e16ac82ae82c9a891ab4652d
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-03-03 08:57:49 +1100

    Use lodash

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 const { validate, User } = require('../models/user');
 
 const Router = express.Router();
@@ -11,14 +12,11 @@ Router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered!');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 
 });
 
