1b7466c22985dd4c14870505c845a7e8d0954447
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-03-03 09:45:45 +1100

    Store Passwords in DB as encrypted passwords

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { validate, User } = require('../models/user');
 
@@ -13,6 +14,8 @@ Router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered!');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
