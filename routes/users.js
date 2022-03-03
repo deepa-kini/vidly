bb2cee458e7528e16d35ff06e15022d9374c23db
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-03-03 12:04:45 +1100

    Endpoint to get the current user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { validate, User } = require('../models/user');
+const auth = require('../middleware/auth');
 
 const Router = express.Router();
+
+Router.get('/me', auth, async (req, res) => {
+  let user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+
+});
 
 
 Router.post('/', async (req, res) => {
