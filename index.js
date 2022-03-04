51da51b2ad749954939477d7389bb031ab7877b4
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-03-04 14:30:55 +1100

    Refactor: Move routes setup to its own module

--- index.js
+++ index.js
@@ -7,35 +7,14 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('config');
 const logger = require('./middleware/logger');
-const error = require('./middleware/error');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const homepage = require('./routes/homepage');
-const Joi = require('joi');
 
+const app = express();
+require('./startup/routes')(app);
+
+const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const port = process.env.PORT || 3000;
-const app = express();
-
-// process.on('uncaughtException', (ex) => {
-//   console.log('We got an uncaught exception!');
-//   winston.error(ex.message, ex);
-//   process.exit(1);
-// })
-
-// process.on('unhandledRejection', (ex) => {
-//   console.log('We got an unhandled rejection!');
-//   winston.error(ex.message, ex);
-//   process.exit(1);
-// })
-
-const p = Promise.reject(new Error('Failed'));
-p.then(() => console.log('Done'));
 
 const dbPassword = config.get('database.password');
 if (!config.get('jwt.jwtPrivateKey') || !dbPassword) {
@@ -54,7 +33,6 @@ winston.add(new winston.transports.MongoDB({ db: dbConnect, level: 'error' }))
 app.set('view engine', 'pug');
 
 // Built-in Middleware functions
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
@@ -69,16 +47,6 @@ if (app.get('env') === 'development') {
 app.use(logger);
 
 // Establish the routes
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/', homepage);
-
-// Error middleware
-app.use(error);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
