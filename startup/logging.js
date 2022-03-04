3f6a0ea190acd3f91c108194944c3d3cf71312b3
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-03-04 15:12:11 +1100

    Commented out the File logging

--- startup/logging.js
+++ startup/logging.js
@@ -6,7 +6,7 @@ const logger = require('../middleware/logger');
 
 module.exports = function (app) {
 
-  winston.add(new winston.transports.File({ filename: 'logfile.log', handleExceptions: true }));
+  // winston.add(new winston.transports.File({ filename: 'logfile.log', handleExceptions: true }));
   winston.add(new winston.transports.Console({ colorize: true, prettyPrint: true }));
 
   if (app.get('env') === 'development') {
